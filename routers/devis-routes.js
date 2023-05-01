0ba04639f857d5c358a2b66f0757a269ec38c4e6
[Ayoub] <[ayoublzr93@gmail.com]>
2023-05-01 21:57:37 +0200

    update_devis

--- routers/devis-routes.js
+++ routers/devis-routes.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const route = express.Router()
-const db = require ('../models')
+const express = require('express');
+const route = express.Router();
+const db = require ('../models');
 const jwt = require('jsonwebtoken');
 const jwtMiddleware = require('express-jwt');
 
