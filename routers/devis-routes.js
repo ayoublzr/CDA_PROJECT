25b1d390221c42fa79e44307ccab01c21309189f
[Ayoub] <[ayoublzr93@gmail.com]>
2023-05-07 22:04:42 +0200

    update_devis_routes

--- routers/devis-routes.js
+++ routers/devis-routes.js
@@ -2,21 +2,37 @@ const express = require('express');
 const route = express.Router();
 const db = require ('../models');
 const jwt = require('jsonwebtoken');
-const jwtMiddleware = require('express-jwt');
 
 
 
 
 
-route.post('/api/devis', jwtMiddleware({ secret:'your-secret-key' }),(req,res,next) => {
-    db.Devis.create({
-        categorie: req.body.categorie,
-        product: req.body.product,
-        surface: req.body.surface,
-        description:req.body.description
-    })
-    .then((response) =>res.status(200).send(response) )
-    .catch((err)=>res.status(400).send(err))
-})
+  const privateKey = process.env.PRIVATE_KEY;
+
+  route.post('/api/devis', (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+    
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, privateKey, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ message: 'Unauthorized' });
+      } else {
+        const userId = decoded.id;
+        db.Devis.create({
+          categorie: req.body.categorie,
+          product: req.body.product,
+          surface: req.body.surface,
+          description: req.body.description,
+          UserId: userId
+        })
+        .then(response => res.status(200).send(response))
+        .catch(err => res.status(400).send(err));
+      }
+    });
+  });
+  
 
 module.exports = route
